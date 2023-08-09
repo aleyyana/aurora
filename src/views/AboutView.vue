ccb97dc7a355cb4fa67ad842eb1d73dfba30a843
Aleyana <annesophie.elsayyed.13560@stu.he-ferrer.eu>
2023-08-09 15:17:52 +0200

    more about page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,12 +1,15 @@
 <template>
 
   <div class="container">
+    <div class="main-title">
+      <h1>{{ title }}</h1>
+    </div>
     <div class="row">
       <div class="col">
         <p></p>
       </div>
       <div class="col">
-        <img src="" alt="">
+        <img src="../assets/img/logo_2.png" alt="">
       </div>
       <div class="col"></div>
     </div>
@@ -16,11 +19,22 @@
 <script>
 
   export default{
+    data(){
+      return{
+        title : "About Aurora"
+      }
+      
+    }
 
   };
 </script>
 
 <style scoped>
 
+h1{
+  font-family: Lust-sans, sans-serif;
+  text-align: center;
+  font-size: var(--header-size);
+}
 
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,22 +5,22 @@
   <div class="container-fluid">
       <div class="main-title">
         <h1> {{ title }}</h1>
-        <!-- <div class="blank-space2"></div> -->
+        <div class="blank-space2"></div>
         <h4 class="quote">Astrology is like gravity. You don't have to believe in it for it to be working in your life.
           <br>
           <span class="author">ZOLAR</span>
         </h4>
       </div>
-      <!-- <div class="blank-space2"></div> -->
+      <div class="blank-space2"></div>
       <div class="center-circle"> 
         <img class="main-image" src="../assets/img/circle.png" alt=""> 
       </div> 
 
-      <!-- <div class="blank-space2"></div> -->
+      <div class="blank-space2"></div>
 
       <div class="center-content" id="anchor1"><a href="#anchor-content">Jump to section 2</a></div>
   </div>
-  <!-- <div class="blank-space2"></div> -->
+  <div class="blank-space2"></div>
   
 </template>
 
