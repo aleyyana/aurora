678df12c2c52b4a227b0a6390d5828599b337c27
Aleyana <annesophie.elsayyed.13560@stu.he-ferrer.eu>
2023-08-19 15:51:33 +0200

    quote daily

---
+++ src/components/QuoteComponent.vue
@@ -0,0 +1,35 @@
+<template>
+
+<section class="quote">
+    <div class="quote-body">
+      {{ quote.text }}
+    </div>
+    <div class="quote-author">
+      {{ quote.author === null ? 'Unknown' : quote.author }}
+    </div>
+  </section>
+    
+</template>
+
+<script>
+
+
+export default{
+
+  data(){
+    return{
+      data:{}
+    }
+  },
+  methods : {
+
+  },
+
+  props: ["quote"],
+}
+
+</script>
+
+<style scoped>
+
+</style>
--- src/views/HoroView.vue
+++ src/views/HoroView.vue
@@ -22,224 +22,146 @@
           <!-- check astro  -->
           <div class="col">
               <div class="row grid gap-2">
-                  <div class="col-xs text-center"> <h6 class="">Aquarius</h6>
+                  <div class="col-xs text-center">
                     <br>
                   <img src="../assets/img/aquarius.png">
                     <br/>
-                  <button class="btn btn-light"  @click="toggleAquarius" ref="AquabtnToggle" >{{ button.text }}</button>
+                  <button class="btn btn-light"  @click="getQuote" ref="" > Aquarius</button>
                   </div>
-                  <div class="col-xs text-center"> <h6 class="">Pisces</h6>
+                  <div class="col-xs text-center">
                     <br>
                   <img src="../assets/img/pisces.png">
                     <br/>
-                  <button class="btn btn-light"  @click="togglePisces" ref="PiscestnToggle" >{{ button.text }}</button>
+                  <button class="btn btn-light"  @click="getQuote" ref="PiscestnToggle" > Pisces</button>
                   </div>
-                  <div class="col-xs text-center"> <h6 class="">Aries</h6>
+                  <div class="col-xs text-center">
                     <br>
                   <img src="../assets/img/aries.png">
                     <br/>
-                  <button class="btn btn-light"  @click="toggleAries" ref="AriesbtnToggle" >{{ button.text }}</button>
+                  <button class="btn btn-light"  @click="getQuote" ref="AriesbtnToggle" > Aries</button>
                   </div>
               </div>
               <div class="row my-2">
-                  <div class="col-xs text-center"> <h6 class="">Taurus</h6>
+                  <div class="col-xs text-center">
                     <br>
                   <img src="../assets/img/taurus.png">
                     <br/>
-                  <button class="btn btn-light"  @click="toggleTaurus" ref="TaurusbtnToggle" >{{ button.text }}</button>
+                  <button class="btn btn-light"  @click="getQuote" ref="TaurusbtnToggle" > Taurus </button>
                   </div>
                   <div class="col-xs text-center">
-                  <h6 class="">Gemini</h6>
+  >
                   <br>
                   <img src="../assets/img/gemini.png">
                 <br/>
-                  <button class="btn btn-light"  @click="toggleGemini" ref="GeminibtnToggle" >{{button.text}}</button>
+                  <button class="btn btn-light"  @click="toggleGemini" ref="GeminibtnToggle" > Gemini </button>
                 </div>
                 <div class="col-xs text-center">
-                  <h6 class="">Cancer</h6>
+  >
                   <br>
                   <img src="../assets/img/cancer.png">
                 <br/>
-                  <button class="btn btn-light"  @click="toggleCancer" ref="CancerbtnToggle" >{{button.text}}</button>
+                  <button class="btn btn-light"  @click="toggleCancer" ref="CancerbtnToggle" >Cancer</button>
                 </div>
               </div>
               <div class="row">
                 <div class="col-xs text-center">
-                  <h6 class="">Leo</h6>
                   <br>
                   <img src="../assets/img/leo.png">
                 <br/>
-                  <button class="btn btn-light"  @click="toggleLeo" ref="LeobtnToggle">{{button.text}}</button>
+                  <button class="btn btn-light"  @click="getQuote" ref="LeobtnToggle">Leo</button>
                 </div>
                 <div class="col-xs text-center">
-                  <h6 class="">Virgo</h6>
+>
                   <br>
                   <img src="../assets/img/virgo.png">
                 <br/>
-                  <button class="btn btn-light"  @click="toggleVirgo" ref="VirgobtnToggle">{{button.text}}</button>
+                  <button class="btn btn-light"  @click="getQuote" ref="VirgobtnToggle">Virgo</button>
                 </div>
                 <div class="col-xs text-center">
-                  <h6 class="">Libra</h6>
+>
                   <br>
                   <img class="pb-2" src="../assets/img/libra.png">
                 <br/>
-                  <button class="btn btn-light"  @click="toggleLibra" ref="LibrabtnToggle">{{button.text}}</button>
+                  <button class="btn btn-light"  @click="getQuote" ref="LibrabtnToggle">Libra</button>
                 </div>
               </div>
               <div class="row">
                 <div class="col-xs text-center">
-                  <h6 class="">Scorpio</h6>
                   <br>
                   <img src="../assets/img/scorpio.png">
                 <br/>
-                  <button class="btn btn-light"  @click="toggleScorpio" ref="ScorpiobtnToggle">{{button.text}}</button>
+                  <button class="btn btn-light"  @click="getQuote" ref="ScorpiobtnToggle">Scorpio</button>
                 </div>
                 <div class="col-xs text-center">
-                  <h6 class="">Sagittarius</h6>
                   <br>
                   <img class="pb-2" src="../assets/img/sagittarius.png">
                 <br>
-                  <button class="btn btn-light"  @click="toggleSagittarius" ref="SagitbtnToggle">{{button.text}}</button>
+                  <button class="btn btn-light"  @click="getQuote" ref="SagitbtnToggle">Sagittarius</button>
                 </div>
                 <div class="col-xs text-center">
-                  <h6 class="">Capricorn</h6>
                   <br>
                   <img src="../assets/img/capricorn.png">
                 <br>
-                  <button class="btn btn-light"  @click="toggleCapricorn" ref="CapribtnToggle">{{button.text}}</button>
+                  <button class="btn btn-light"  @click="getQuote" ref="CapribtnToggle">Capricorn</button>
                 </div>
               </div>
           </div>
           <!-- result astro  -->
           <div class="col">
 
-            <div class="col-12">
-            <transition name="fade">
-            <aquarius-sign
-            v-show="showAquarius"
-            />        
-        </transition>
-      </div>
+            <QuoteComponent :quote="quote" />
+            <div class="button-container">
+            </div>
           </div>
         </div>
-
-
     </div>
 </template>
 
 <script>
 
-import AquariusSign from '../components/aquasign.vue';
+import QuoteComponent from '../components/QuoteComponent.vue';
 
 export default {
-
-    data(){
-
-      return{
-        button:{
-          text: 'Today'
+  data(){
+    return{
+      quote:{
+        text:"",
+        author: "",
+        button: {
+          text:'Today'
         },
-        showAquarius:false,
-
+        quotes: []
       }
-    },
-
-    components: {
-      AquariusSign
+    }
   },
 
-  methods : {
-    zodiac : function zodiac(){
-    var birthmonth = document.getElementById("month").value;
-    var birthday = document.getElementById("day").value;
-    var result = " unknown because you didn't put a valid date.";
+  components: {
+    QuoteComponent
+  },
 
+  created(){
+    this.getQuote();
+  },
 
-    if (birthmonth == 1&& birthday >= 20 || birthmonth == 2 && birthday <= 18
-    ){
-        result = ("Aquarius");
-     document.getElementById("photo").innerHTML="<img src='../assets/img/hands_aquarius.png'width=20%>";
-      document.getElementById("characteristic").innerHTML="Aquarians are extremely vulnerable and sensitive. Although you may often find them being surrounded by many friends but in reality they rarely have close friends and acquaintances. Aquarius is a universal sign which makes them public people. Hence Aquarians are often associated with clubs, organizations and forums and enthusiastically participate in intellectual discussions. Aquarians are great communicators as long as they are within their mental realm.";
-  
-    }
+  methods:{
+    async getQuote() {
+      const data = await fetch("https://type.fit/api/quotes").then(res => res.json());
+      const randomQuote = Math.floor(Math.random() * data.length);
+      if (this.quote.content) {
+        this.quotes = [...this.quotes, this.quote];
+      }
 
-    if (birthmonth == 2&& birthday >= 19 || birthmonth == 3&& birthday <= 20){
-       result = ("Pisces");
-       document.getElementById("photo").innerHTML="<img src='https://www.horoscope.com/images-US/signs/profile-pisces.png'width=20%>";
-      document.getElementById("characteristic").innerHTML="Pisceans live in their imaginary world that barely has a connection with the reality. They love to look at the world through a rosy window. When challenged by reality, Pisceans have the tendency to retreat into their world of imagination.";
-      
-    }
-   if (birthmonth == 3&& birthday >= 21 || birthmonth == 4 && birthday <= 19){
-       result = ("Aries");
-      document.getElementById("photo").innerHTML="<img src='https://www.horoscope.com/images-US/signs/profile-aries.png'width=20%>";
-     document.getElementById("characteristic").innerHTML="Aries demonstrate strong personality. They have strong leadership qualities and honest and straightforward. Aries often have strong determination and can’t be deterred by failures.Aries are always eager for action. They take up to leadership spontaneously. But they don’t judge the pros and cons of a situation before acting. This also makes them vulnerable.";
-   }
-  
-    if (birthmonth == 4&& birthday >= 20|| birthmonth == 5 && birthday <= 20){
-        result = ("Taurus");
-       document.getElementById("photo").innerHTML="<img src='https://www.horoscope.com/images-US/signs/profile-taurus.png'width=20%>";
-       document.getElementById("characteristic").innerHTML="Taureans are noted for their determination and zeal. It is not easy to distract a Taurian from his goal once he has set his target. He would stay focused on his target and would continuously strive to achieve it.Taureans attach high value to simplicity and functionality. They often live a life that is simple and devoid of luxury.";
-      
-    }
-  
-    if (birthmonth == 5&& birthday >= 21 || birthmonth == 6 && birthday <= 20
-     ){
-        result = ("Gemini");
-       document.getElementById("photo").innerHTML="<img src='https://www.horoscope.com/images-US/signs/profile-gemini.png'width=20%>";
-      document.getElementById("characteristic").innerHTML="Geminis are full of duality. They always look to a situation from dual perspective. Geminis are characterized by inconstancy and dual nature. Geminis therefore always stay confused about their feelings.Geminis however are strong communicators and express good control over language. They are often found to have knowledge over several languages.";
-     
-    }
-  
-    if (birthmonth == 6&& birthday >= 21 || birthmonth == 7&& birthday <= 22){
-        result = ("Cancer");
-       document.getElementById("photo").innerHTML="<img src='https://www.horoscope.com/images-US/signs/profile-cancer.png'width=20%>";
-      document.getElementById("characteristic").innerHTML="Cancerians are emotional. Their lives are often inflicted with mood shifts. You can find a Canerian in different moods even during a day.The true emotion of Cancer however is hidden behind their composure. But they are soft creatures and can be hurt easily by unkind words.People of the Cancer zodiac sign can be prone to depression and other mental issues. However, cancers are great family people and enjoy big families around them.";
-      
-    }
-    if (birthmonth == 7&& birthday >= 23 || birthmonth == 8 && birthday <= 22){
-        result = ("Leo");
-       document.getElementById("photo").innerHTML="<img src='https://www.horoscope.com/images-US/signs/profile-leo.png'width=20%>";
-            document.getElementById("characteristic").innerHTML="Leos are warm spirited. They are full of energy and always eager to jump into action. Leos crave for recognition and admiration. Leos always love to be at the centre of attraction. They strive to reach to the top in whatever they do. Leos always love to be surrounded with large crowd and admirers. However, they are often unkind to criticism and don’t take the words of critics lightly.Leos are very ambitious and choose their acquaintances carefully. They aspire for social recognition.";
-      
-    }
-    if (birthmonth == 8 && birthday >= 23 || birthmonth == 9 && birthday <= 22){
-        result = ("Virgo");
-       document.getElementById("photo").innerHTML="<img src='https://www.horoscope.com/images-US/signs/profile-virgo.png'width=20%>";
-        document.getElementById("characteristic").innerHTML="Virgos have a keen sense of good and bad and for that they are highly discriminating. They have an intuitive sense to identify wrong motives in people. Hence, Virgos exercise extreme caution in what they do.The virgin defines purity and therefore they are endowed with the ability to distinguish the good from the bad. Hence, Virgos are also cleanliness freaks. Despite their intelligence Virgos often remain confused about the decisions they make in their life. Also, you will not find Virgos teeming with activities.";
-    }
-    if (birthmonth == 9&& birthday >= 23 || birthmonth == 10&& birthday <= 22){
-      result = ("Libra");
-       document.getElementById("photo").innerHTML="<img src='https://www.horoscope.com/images-US/signs/profile-libra.png'width=20%>";
-        document.getElementById("characteristic").innerHTML="Libra is an active sign and members born under the sign are endowed with high energy. But Librans also tend to run out of their energy soon.The Libra is the ‘balance’ and members of this sign have a very balanced mind. They can be found in settling disputes often. Librans always try to maintain harmony and balance. They are also very level headed and have a keen sense for justice. Therefore, their suggestions are often highly sought after by their friends and kins.Librans are two faceted characters. They both have the cheerfulness and darkness in them. Librans have phases of heightened activities but they can easily slip into a phases of complete inactivity and apathy as well.";
-    }
-    if (birthmonth == 10 && birthday >= 23|| birthmonth == 11 && birthday <= 21){
-       result = ("Scorpio");
-       document.getElementById("photo").innerHTML="<img src='https://www.horoscope.com/images-US/signs/profile-scorpio.png'width=20%>";
-        document.getElementById("characteristic").innerHTML="Scorpions are most diverse in nature and therefore present the most interesting study. Scorpios hold grudge and would wait patiently for the right moment to strike. They are not likely to forget any act of betrayal or treachery. For Scorpios what is implied is more fascinating than the obvious.Scorpios are often described as egoists. However, some of the positive traits of Scorpios are- diplomacy, intuition, intelligence, engaging, resolute, spirituality and sensitivity.";
-    }
-    if (birthmonth == 11 && birthday >= 22 || birthmonth == 12 && birthday <= 21){
-        result = ("Sagittarius");
-       document.getElementById("photo").innerHTML="<img src='https://www.horoscope.com/images-US/signs/profile-sagittarius.png'width=20%>";
-        document.getElementById("characteristic").innerHTML="Sagittarians are the incurable optimists. They are always looking at the positive side of a thing. Their optimism can’t be dampened by hardships or negative results.Sagittarians often are outdoor people. They will take interest in all sorts of sports and outdoor activities. They are also adventures in nature. Although they are true to their faults but can also turn completely deaf to criticism and turn down suggestions.";
-    }
-  
-  if(birthmonth == 12 && birthday >= 22 || birthmonth == 1 && birthday <= 19){
- 
-        result = ("Capricorn");
-     document.getElementById("photo").innerHTML="<img src='https://www.horoscope.com/images-US/signs/profile-capricorn.png'width=20%>";
-     document.getElementById("characteristic").innerHTML="Members of this sign has an insatiable desire to climb higher and during this course they can also become selfish and might not hesitate to sacrifice other’s interests in fulfilling their goals.Capricorns may seem risk averse but in reality they carefully plan all their moves ahead and rehearse them to perfection.";
-    }else if (birthmonth>12||birthday>31){
-       alert("Please enter an accurate date ! ");
-    }
- document.getElementById("output").innerHTML= result;
-    },
+      this.quote = {
+        text: data[randomQuote].text,
+        author: data[randomQuote].author
+      };
 
-  toggleAquarius () {
-    this.showAquarius = !this.showAquarius;
-    this.$refs.AquabtnToggle.innerText = this.showAquarius?'Hide':'Today';
     }
   }
-};
+}
+
+
+
 
 
 </script>
